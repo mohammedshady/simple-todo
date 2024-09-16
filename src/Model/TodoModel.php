69269edcd9256ba66df53935f2454598441f2a45
Mohammed Shady Elsayed <75118102+mohammedshady@users.noreply.github.com>
2024-09-16 15:12:25 +0300

    added user-todo relation in models

--- src/Model/TodoModel.php
+++ src/Model/TodoModel.php
@@ -2,21 +2,29 @@
 
 namespace App\Model;
 
+use App\Model\UserModel;
+
 class TodoModel
 {
     public int $id;
     public string $title;
     public bool $completed;
+    public array $users = [];
 
     public function __construct(array $data = [])
     {
-        if (!isValid($data)) return;
+        if (!$this->isValid($data)) return;
 
         $this->id = $data['id'] ?? 0;
         $this->title = $data['title'] ?? '';
         $this->completed = $data['completed'] ?? false;
     }
-
+    public function setUsers(array $users): void
+    {
+        $this->users = array_map(function ($userData) {
+            return $userData instanceof UserModel ? $userData : new UserModel($userData);
+        }, $users);
+    }
     public function isValid (array $data = []): bool{
 
         // handle validation logic
--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -6,14 +6,21 @@ class UserModel
     public int $id;
     public string $username;
     public string $email;
+    public array $todos = [];
 
     public function __construct(array $data = [])
     {
-        if (!isValid($data)) return;
+        if (!$this->isValid($data)) return;
 
         $this->id = $data['id'] ?? 0;
         $this->username = $data['username'] ?? '';
         $this->email = $data['email'] ?? '';
+    }
+    public function setTodos(array $todos): void
+    {
+        $this->todos = array_map(function ($todoData) {
+            return $todoData instanceof TodoModel ? $todoData : new TodoModel($todoData);
+        }, $todos);
     }
     public function isValid (array $data = []): bool{
 
